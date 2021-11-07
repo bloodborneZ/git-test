29cdd8f76005750bd3bf9219ffd47a720a3cc878
whx <867345990@qq.com>
2021-11-07 15:23:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author whx
+ * @create 2021-11-07 15:12
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
